1fb20bb28f224f26b966c1b498b237cdeb000e6c
Alexander Paul <alexander.paul@apus.at>
2022-11-23 10:33:10 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+
+for (var i = 0; i < 10; i++)
+{
+	CalculateChanges();
+}
+
+static void CalculateChanges()
+{
+	var tree1 = GetTree("tree1.vb");
+	var tree2 = GetTree("tree2.vb");
+
+	// Subsequent calls of GetChanges lead to potentially different results
+	var treeChanges = tree2.GetChanges(tree1);
+
+	Console.WriteLine($"Number of changes: {treeChanges.Count}");
+}
+
+static SyntaxTree GetTree(string file)
+{
+	return VisualBasicSyntaxTree.ParseText(File.ReadAllText(file));
+}
